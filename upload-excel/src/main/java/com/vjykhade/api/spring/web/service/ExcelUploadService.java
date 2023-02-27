bcb7289c7867e3f4e15d857b761b3b0ccce131be
Vijay Khade <61784658+vjykhade@users.noreply.github.com>
2023-02-27 12:05:58 +0530

    Add files via upload

---
+++ upload-excel/src/main/java/com/vjykhade/api/spring/web/service/ExcelUploadService.java
@@ -0,0 +1,36 @@
+package com.vjykhade.api.spring.web.service;
+
+import com.vjykhade.api.spring.web.entity.CurrencyUpload;
+import com.vjykhade.api.spring.web.helper.ExcelHelper;
+import com.vjykhade.api.spring.web.repository.CurrencyUploadRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ExcelUploadService     {
+
+    @Autowired
+    CurrencyUploadRepository currencyUploadRepository;
+    public void saveDataInDB(MultipartFile file) {
+        try {
+            List<CurrencyUpload> currencyUploadList = ExcelHelper.excelToDBUpload(file.getInputStream());
+            currencyUploadRepository.saveAll(currencyUploadList);
+        } catch (IOException e) {
+            throw new RuntimeException("Fail to upload data into the db: " + e.getMessage());
+        }
+    }
+
+    public List<CurrencyUpload> getAllCurrencyDetails() {
+        return currencyUploadRepository.findAll();
+    }
+
+    public Optional<CurrencyUpload> getOneCurrencyDetails(Long id)
+    {
+        return currencyUploadRepository.findById(id);
+    }
+}
